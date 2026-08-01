5c87ba246e89a57a45ae97dbc1612cd545ea757e
Ruturaj <rhuturajvyavahare95.com>
2026-08-01 23:59:03 +0530

    Add OR Gate

---
+++ 01_Basic_Gates/commands.v
@@ -0,0 +1,9 @@
+//iverilog -o mux mux2_1.v mux2_1_tb.v
+//vvp a.out
+//gtkwave mux.vcd
+
+//iverilog -o and_gate and_gate.v and_gate_tb.v
+//vvp and_gate
+//gtkwave and_gate.vcd
+
+//
--- 01_Basic_Gates/or_gate.v
+++ 01_Basic_Gates/or_gate.v
@@ -1,9 +1,9 @@
-module and_gate(
+module or_gate(
     input wire a,
     input wire b,
     output wire y
 );
 
-    assign y = a & b;
+    assign y = a | b;
 
 endmodule
